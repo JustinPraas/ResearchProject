d5ccf8d4eac85ba34fef57a05a3ec9964fb5b078
Justin Praas <jw.praas@gmail.com>
2019-12-17 08:24:43 +0100

    Large heatmap singles it500 rff

---
+++ large_hm_betweenness_it500_RFF.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from main import centralities, generateHeatmaps, single_combs
+generateHeatmaps(["betweenness"], [("betweenness")], [0.01, 0.05, 0.1, 0.15], 500, [50, 100, 200, 400], [80, 40, 20, 10])
