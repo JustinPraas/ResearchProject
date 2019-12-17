d49c5749d9698561d4b5f35000057f6d598f41fb
JustinPraas <j.w.praas@student.utwente.nl>
2019-12-17 06:52:54 +0100

    Work out sbatch

--- job.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH -p m610
-#SBATCH -J test_job
-#SBATCH -N2
-#SBATCH -C16
-#SBATCH --mail-type=END,FAIL       # email status changes
-srun -N1 -n1 -c16 --slurmd-debug=4 -o main.txt main.sh
-srun -N1 -n1 -c16 --slurmd-debug=4 --mail-user -o small_heatmaps.txt small_heatmaps.sh
-
